317fe505f25570bfa3a2c04be41897e15b55e735
osfunapps <osfunapps@gmail.com>
2021-05-30 19:10:12 +0300

    s

---
+++ Classes/GenericQueue.swift
@@ -0,0 +1,64 @@
+//
+//  CompletableSemaphore.swift
+//  GeneralStreamiOS
+//
+//  Created by Oz Shabat on 13/07/2020.
+//  Copyright © 2020 osCast. All rights reserved.
+//
+
+import Foundation
+
+/**
+ This class represents a generic queue with a flushing mechanism.
+ Use it if you want to
+ */
+public class GenericQueue<T> {
+    
+    // queue
+    public var queue = [T]()
+    private var isFlushing = false  // flushing indicator
+    private var queueDispatchQueue: DispatchQueue!  // the associated dispatch queue
+    public var delegate: GenericQueueDelegate<T>? = nil // an optional delegate
+    
+    public init(dispatchQueueName: String = "queue_dq") {
+        queueDispatchQueue = DispatchQueue(label: dispatchQueueName, qos: .utility)
+    }
+    
+    /// Will add another item to the queue. The queue will be flushed by default
+    public func addToQueue(item: T, toFlush: Bool = true) {
+        queue.append(item)
+        if toFlush {
+            flush()
+        }
+    }
+    
+    // MARK: - flush types
+    private func flush() {
+        if isFlushing {
+            return
+        }
+        
+        isFlushing = true
+        flushNextRequest()
+    }
+    
+    private func flushNextRequest() {
+        queueDispatchQueue.async {
+            guard let item = self.queue.popLast() else {
+                self.isFlushing = false
+                return
+            }
+            // report flush
+            self.delegate?.GenericQueueDidFlush(item: item)
+            
+            // run recursively again
+            self.flushNextRequest()
+        }
+    }
+}
+
+/// Implement this delegate to get reports about any event related to the generic queue (when flush and more)
+public class GenericQueueDelegate<T> {
+    public func GenericQueueDidFlush(item: T){}
+}
+
