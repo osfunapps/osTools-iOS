5c1f2616756a9baff0a24118910638ee0356c97b
osapps <admin@os-apps.com>
2024-04-18 10:55:32 +0300

    s

--- Classes/GenericListItem.swift
+++ Classes/GenericListItem.swift
@@ -12,13 +12,19 @@ public struct GenericListItem: Codable {
     public var name: String
     public var id: String?
     public var imgRes: String?
-    public init(name: String, id: String?, imgRes: String? = nil) {
+    public var optionalValue: String?
+    
+    public init(name: String,
+                id: String?,
+                imgRes: String? = nil,
+                optionalValue: String? = nil) {
         self.name = name
         self.id = id
         self.imgRes = imgRes
+        self.optionalValue = optionalValue
     }
     
     public func copy() -> GenericListItem {
-        return GenericListItem(name: name, id: id, imgRes: imgRes)
+        return GenericListItem(name: name, id: id, imgRes: imgRes, optionalValue: optionalValue)
     }
 }
